7a9bc3aea426ffd6d9cb1458e6998857669e9ebc
WillieYang <849346604@qq.com>
2017-01-31 19:59:17 +0000

    Chapter Four with Exercise

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,6 @@ def index(request):
 	context_dict = {'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
 	return render(request, 'rango/index.html', context=context_dict)
 
-def about(requet):
-	link = "Rango says here is the about page" + '<a href = "/rango">Main Page</a>'
-	return HttpResponse(link)
+def about(request):
+	context_dict = {'name':'Sheng Yang'}
+	return render(request, 'rango/about.html', context=context_dict)
