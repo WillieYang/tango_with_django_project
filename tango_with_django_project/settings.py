d94737117f4cc2ac033ff0edfb256693d274212a
WillieYang <849346604@qq.com>
2017-02-12 15:33:49 +0000

    Chapter 11 with Exercise

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
-    'registration'
+    'registration',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -134,9 +134,12 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
-LOGIN_URL = '/rango/login/'
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
+LOGIN_URL = '/accounts/login/'
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
-
 SESSION_COOKIE_AGE = 1209600
 
