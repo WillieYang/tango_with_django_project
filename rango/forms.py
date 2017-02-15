a74009d1472af3258574c2f5ed3b6b219f4fdb46
WillieYang <849346604@qq.com>
2017-02-15 16:25:42 +0000

    beautify the register.html by bootstrap

--- rango/forms.py
+++ rango/forms.py
@@ -4,7 +4,7 @@ from rango.models import Category, Page, UserProfile
 
 class CategoryForm(forms.ModelForm):
 	name = forms.CharField(max_length=128,
-						   help_text="Please enter the category name.")
+						   help_text="Please enter the category name:")
 	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 	likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 	slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -15,9 +15,9 @@ class CategoryForm(forms.ModelForm):
 
 class PageForm(forms.ModelForm):
 	title = forms.CharField(max_length=128,
-						    help_text="Please enter the title of the page.")
-	url = forms.URLField(max_length=200,
-							help_text="please enter the URL of the page.")
+						    help_text="Please enter the title of the page:")
+	url = forms.URLField(max_length=200,	
+							help_text="Please enter the URL of the page:")
 	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
 	class Meta:
