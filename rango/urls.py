406c0c15c7e9b45daf471d75a6b30ec395e46ac8
WillieYang <849346604@qq.com>
2017-02-24 14:07:45 +0000

    update of url.py about track_page

--- rango/urls.py
+++ rango/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
 		views.show_category, name='show_category'),
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
 	url(r'^restricted/$', views.restricted, name='restricted'),
-	url(r'^goto/?page(?P<page_id>\d+)/$', views.track_url, name='goto'),
+	url(r'^goto/(?P<page_id>\d+)/$', views.track_url, name='goto'),
 	url(r'^profile_registration/$', views.register_profile, name='register_profile'),
 ]
