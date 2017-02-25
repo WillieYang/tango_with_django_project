45a7d1fc90e6be3280dfef89d67a35048d3b7f84
WillieYang <849346604@qq.com>
2017-02-25 17:40:23 +0000

    Find a problem in the view of visitor_cookie, that is, when the vist time is to much, the time format would throw the exception

--- static/js/rango-jquery.js
+++ static/js/rango-jquery.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
 	$("#about-btn").dblclick(function(event) {
 		alert("Click the button using JQuery");
 	})
-	$("p").hover(function() {
-		$("this").css('color', 'red');
+	$("#about-btn").addClass('btn btn-primary')
+	$("p").hover(function(event) {
+		$(this).css('color', 'red');
 	},
-	function() {
-		$("this").css('color', 'blue');
+	function(event) { 
+		// event seems can be commented...
+		$(this).css('color', 'blue');
 	})
 });
