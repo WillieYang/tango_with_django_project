a7bc813e1077c1b8ea3462f2f135f4cf1af244c2
WillieYang <849346604@qq.com>
2017-02-25 21:02:42 +0000

    Chapter 16.2...about the JQuery

--- static/js/rango-jquery.js
+++ static/js/rango-jquery.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 	$("#about-btn").dblclick(function(event) {
 		alert("Click the button using JQuery");
-	})
+		msgstr = $("#msg").html()
+		msgstr = msgstr + "ooo"
+		$("#msg").html(msgstr)
+	});
 	$("#about-btn").addClass('btn btn-primary')
 	$("p").hover(function(event) {
 		$(this).css('color', 'red');
@@ -9,5 +12,5 @@ $(document).ready(function(){
 	function(event) { 
 		// event seems can be commented...
 		$(this).css('color', 'blue');
-	})
+	});
 });
